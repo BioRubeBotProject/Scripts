6c972ffff1bb96aeeae04f2ab4cb962c429316f7
onebaldman <onebaldman@gmail.com>
2015-03-20 12:00:54 -0500

    Initial Commit

---
+++ ExtraCellularProperties.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtraCellularProperties : MonoBehaviour
+    {
+    public bool allowMovement = true;
+
+    // Use this for initialization 
+    void Start()
+        {
+        }
+
+    // Update is called once per frame 
+    void Update()
+        {
+        }
+    }
